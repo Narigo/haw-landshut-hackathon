753924e4e38460ff3e9d3b55e409768bacf184ce
konstantinaxt <axtkonstantin@hotmail.de>
2017-10-13 23:52:15 +0200

    add styling to appointment

--- src/dashboard/components/Layout/meta/Layout.story.tsx
+++ src/dashboard/components/Layout/meta/Layout.story.tsx
@@ -4,10 +4,13 @@ import {storiesOf} from "@storybook/react";
 
 import {action} from "@storybook/addon-actions";
 
+import * as moment from "moment";
+
 import Layout from "../Layout";
 import BatteryStatus from "../../BatteryStatus/BatteryStatus";
 import Status from "../../Status/Status";
 import DriverProfile from "../../DriverProfile/DriverProfile";
+import Appointments from "../../Appointments/Appointments";
 import * as portraitImage from "../../../../resources/images/portrait.jpg";
 
 storiesOf("Layout", module)
@@ -16,6 +19,7 @@ storiesOf("Layout", module)
       <Status statusText="Du hast alles im Griff." status="ok">
         <DriverProfile position="right" imgSrc={portraitImage} />
       </Status>
+      <Appointments appointments={[randomAppointment(), randomAppointment(), randomAppointment()].sort((a, b) => a.time.diff(b.time))} />
       <BatteryStatus
         batteryState="ok"
         chargeStatus={76}
@@ -31,6 +35,7 @@ storiesOf("Layout", module)
       <Status statusText="Der Verbrauch neigt sich dem Ende." status="warning">
         <DriverProfile position="right" imgSrc={portraitImage} />
       </Status>
+      <Appointments appointments={[randomAppointment(), randomAppointment(), randomAppointment()].sort((a, b) => a.time.diff(b.time))} />
       <BatteryStatus
         batteryState="warning"
         chargeStatus={37}
@@ -46,6 +51,7 @@ storiesOf("Layout", module)
       <Status statusText="Suchen Sie demnächst dringend eine SmartStation auf." status="error">
         <DriverProfile position="right" imgSrc={portraitImage} />
       </Status>
+      <Appointments appointments={[randomAppointment(), randomAppointment(), randomAppointment()].sort((a, b) => a.time.diff(b.time))} />
       <BatteryStatus
         batteryState="error"
         chargeStatus={18}
@@ -56,3 +62,16 @@ storiesOf("Layout", module)
       />
     </Layout>
   ));
+
+
+const startLat = 48.5560425;
+const startLng = 12.1975011;
+function randomAppointment() {
+  return {
+    text: texts[Math.floor(Math.random() * texts.length)],
+    location: { lat: startLat + (Math.random() * 0.01 - 0.05), lng: startLng + (Math.random() * 0.01 - 0.05) },
+    time: moment(moment.now()).add(Math.random() * 60 * 60 * 24 * 3, "seconds")
+  };
+}
+
+const texts = ["Zahnarzt", "Banktermin", "Café", "Versicherungstermin"];
